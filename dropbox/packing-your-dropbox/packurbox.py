2d8beb64ef1dc54be4f38dbd0ca8e3cf4da07fda
Robert Ramsay <robert.alan.ramsay@gmail.com>
2011-01-31 09:02:46 -0500

    Added documentation to the beginning.
    I need to account for the fracturing of freespace, and update fit so that it will work with a list that isn't presorted or aligned.

--- dropbox/packing-your-dropbox/packurbox.py
+++ dropbox/packing-your-dropbox/packurbox.py
@@ -1,3 +1,28 @@
+#!/usr/bin/python
+"""
+Robert Ramsay <robert.alan.ramsay@gmail.com>
+Packing your Dropbox
+When you're working with petabytes of data, you have to store files wherever they can fit. All of us here at Dropbox are always searching for more ways to efficiently pack data into smaller and more manageable chunks. The fun begins when you bend the rules a little bit and visualize it in two dimensions.
+
+You'll be given a list of rectangular "files" that you'll need to pack into as small a "Dropbox" as possible. The dimensions of each file will be specified by a tuple (width, height), both of which will be integers. The output of your function should be the area of the smallest rectangular Dropbox that can enclose all of them without any overlap. Files can be rotated 90(deg) if it helps. Bonus points if you can draw pictures of the winning configurations along the way. While drawing pictures, any files sharing dimensions should be considered identical/interchangeable.
+
+Input
+Your program must read a small integer N (1 <= N <= 100) from stdin representing the maximum number of files to consider, followed by the width and height of each file, one per line.
+
+Output
+Output should be simply be the area of the smallest containing Dropbox. If you want to print pretty pictures, send that to stderr. Only the output on stdout will be judged.
+
+Sample Input
+
+3
+8 8
+4 3
+3 4
+
+Sample Output
+
+88
+"""
 class DropBox:
     w = 1
     h = 1
@@ -5,7 +30,7 @@ class DropBox:
         t = self.w
         self.w = self.h
         self.h = t
-    
+
     def align(self):
         if self.w > self.h:
             self.rotate()
@@ -14,7 +39,7 @@ class DropBox:
 #free space = (lowest left x, lowest left y, width, height)
 def fit(size, free, box):
     x, y, w, h = free
-    
+
     if h < box.h and w < box.w:
         size = (size[0]+box.w-w, size[1]+box.h-h)
         x += box.w
@@ -28,7 +53,7 @@ def fit(size, free, box):
     else:
         x += box.w
         w -= box.w
-        
+
     free = (x, y, w, h)
     return size, free
 
