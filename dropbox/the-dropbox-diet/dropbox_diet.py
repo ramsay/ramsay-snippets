5366a39a7ec276a8f26ce0879783319af4df9c63
Robert Ramsay <robert.alan.ramsay@gmail.com>
2011-02-07 22:11:30 -0500

    A quick implementation of the subset sum solution from wikipedia. I'm going to change the implementation.

--- dropbox/the-dropbox-diet/dropbox_diet.py
+++ dropbox/the-dropbox-diet/dropbox_diet.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 #!/usr/bin/python
 ''' The Dropbox Diet challenge
 Of the boatload of perks Dropbox offers, the ones most threatening to our engineers' waistlines are the daily lunches, the fully-stocked drink fridge, and a full-length bar covered with every snack you could want. All of those calories add up. Luckily, the office is also well-equipped with ping-pong, a DDR machine, and a subsidized gym right across the street that can burn those calories right back off. Although we often don't, Dropboxers should choose the food they eat to counterbalance the activities they perform so that they don't end up with caloric deficit or excess.
@@ -38,12 +39,20 @@ coding-six-hours
 cookies
 mexican-coke
 '''
+import sys
 
 def sum_activities(tuple_list):
     s = 0
     for t in tuple_list:
         s += t[1]
     return s
+
+def min_activities(tuple_list):
+    v = (None,0)
+    for t in tuple_list:
+        if t[1] < v[1]:
+            v = t
+    return v
 
 def max_activities(tuple_list):
     v = (None,0)
@@ -64,7 +73,7 @@ def diet(activities):
         if v > 0:
             positive.append((k,v))
         elif v < 0:
-            negative.append((k,abs(v)))
+            negative.append((k,v))
         else:
             # Value is zero?! Good for any diet.
             diet_list.append(k)
@@ -75,7 +84,7 @@ def diet(activities):
         j = 0
         for j in range(len(negative)):
             k2, v2 = negative[j]
-            if v == v2:
+            if v == abs(v2):
                 diet_list.append(k)
                 diet_list.append(k2)
                 positive[i] = None
@@ -89,17 +98,17 @@ def diet(activities):
     
     #Remove extremes from the positive side
     m = max_activities(positive)
-    s = sum_activities(negative)
+    s = -1*sum_activities(negative)
     while negative and m[1] > s:
         positive.remove(m)
         m = max_activities(positive)
     
     #Remove extremes from the negative side
-    m = max_activities(negative)
+    m = min_activities(negative)
     s = sum_activities(positive)
     while positive and m[1] > s:
         negitive.remove(m)
-        m = max_activities(negative)
+        m = min_activities(negative)
     
     # At this point all items left will be non extremes and non-trivial
     ''' From en.wikipedia.org/wiki/Suset_sum_problem on Feb. 7, 2011
@@ -113,13 +122,13 @@ def diet(activities):
         "there is a nonempty subset of x1, ..., xi which sums to s".
     Thus, the solution to the problem is the value of Q(n,0).
     Clearly, Q(i,s) = false if s < N or s > P so these values do not need to be
-    stored or computed. Create an array to hold the values Q(i,s) for 1 ≤ i ≤ n
-    and N ≤ s ≤ P.
-    The array can now be filled in using a simple recursion. Initially, for 
-    N ≤ s ≤ P, set
-    Q(1,s) := (x1 = s).
-    Then, for i = 2, …, n, set
-    Q(i,s) := Q(i − 1,s) or (xi = s) or Q(i − 1,s − xi)   for N ≤ s ≤ P.
+    stored or computed. Create an array to hold the values Q(i,s) for 1 <= i <= n
+    and N <= s <= P.
+    The array can now be filled in using a simple recursion. 
+    Initially, for N <= s <= P, set
+        Q(1,s) := (x1 = s).
+    Then, for i = 2, ..., n, set
+        Q(i,s) := Q(i − 1,s) or (xi = s) or Q(i − 1,s − xi) for N <= s <= P.
     For each assignment, the values of Q on the right side are already known, 
     either because they were stored in the table for the previous value of i or
     because Q(i − 1,s − xi) = false if s − xi < N or s − xi > P. Therefore, the
@@ -132,6 +141,26 @@ def diet(activities):
     number of bits used to represent it. This algorithm is polynomial in the 
     values of N and P, which are exponential in their numbers of bits.
     '''
+    N = sum_activities(negative)
+    P = sum_activities(positive)
+    both = negative + positive
+    q = [[both[0][1] == s for x in range(N,P)]]
+
+    for i in range(1,len(both)):
+        l = [False]*(P-N)
+        for s in range(N,P):
+            if both[i][1] == s:
+                l[s-N] = True
+            else:
+                l[s-N] = q[i-1][s-N]
+                l[s-N] = l[s-N] or q[i-1][s-both[i][1]-N]
+            l[s-N] = False
+        q.append(l)
+        #q.append([ q[i-1][s-N] or both[i][1] == s or q[i-1][s-both[i][1]-N] for s in range(N,P)])
+        
+    for i in range(len(both)-1,0,-1):
+        if q[i][0-N]:
+            return "yes, " + str(i)
     
     if diet_list:
         #We have at least one entry.
