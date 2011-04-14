3ba7d0d6d29bdff6ef4690b1410b70f8bd43a43e
Robert Ramsay <robert.alan.ramsay@gmail.com>
2011-04-14 01:42:46 -0400

    Added a test method for the actual puzzle using the given input and output.

--- facebook/testbreathalyzer.py
+++ facebook/testbreathalyzer.py
@@ -6,6 +6,15 @@ def TestDistance():
     assert(breathalyzer.distance("ab", "a") == 1)
     assert(breathalyzer.distance("cat", "cot") == 1)
     assert(breathalyzer.distance("aaaa", "bbbb") == 4)
+    assert(breathalyzer.distance("GOOD", "GOUD") == 1)
+    assert(breathalyzer.distance("GOUD", "GOOD") == 1)
+
+
+def TestBreathalyzer():
+    wordlist = open("twl06.txt")
+    post = open("breathalyzer.txt").read()
+    assert(breathalyzer.breathalyzer(post, wordlist)==8)
 
 if __name__ == '__main__':
     TestDistance()
+    TestBreathalyzer()
